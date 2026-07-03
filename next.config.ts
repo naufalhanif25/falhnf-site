4f3a5af22662915252b963c31fc10395c8b57e9e
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-07-03 12:09:52 +0700

    chore(config): remove allowedDevOrigins from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
     reactCompiler: true,
     devIndicators: false,
-    allowedDevOrigins: ["172.24.224.1"]
 }
 
 export default nextConfig
